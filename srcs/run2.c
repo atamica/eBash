5cd05219468f9e134cd9361e644efa4b31f3ae41
atamica <atamica@student.21-school.ru>
2021-12-01 09:39:08 +0300

    pip1

---
+++ srcs/run2.c
@@ -0,0 +1,40 @@
+#include "minishell.h"
+
+void	cmd1_cmd(t_d *d, t_cmd *cmd)
+{
+	if (cmd->path)
+	{
+		if (execve(cmd->path, cmd->arg, cmd->env) == -1)
+		{
+			perror(MSGE8);
+			strerror(errno);
+			exit(errno);
+		}
+		exit(0);
+	}
+	err(8, d);
+}
+
+void	wait_chailds_cmd(t_d *d)
+{
+	int	res;
+
+	res = waitpid(d->cmd_pid, &d->stat, 0);
+	if (res < 0)
+		out_msg(MSGE7, 0);
+	if (d->stat)
+		out_msg(MSGE8, 0);
+	d->cmd.code_ret = res;
+}
+
+
+int	cmd_cmd(t_d *d, t_cmd *cmd)
+{
+	d->cmd_pid = fork();
+	if_err_exit(d->cmd_pid, 5, d);
+	if (d->cmd_pid)
+		wait_chailds_cmd(d);
+	else
+		cmd1_cmd(d, cmd);
+	return (d->cmd.code_ret);
+}
